878debc8d34486c02f81a4705739cfa4e23fc27e
TheDigitalMazee <amrelbaz@hotmail.com>
2025-07-16 09:02:50 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jira-search-app",
+    version="0.1",
+    install_requires=[
+        "streamlit>=1.28.0",
+        "sentence-transformers>=2.2.0",
+        # other dependencies
+    ],
+)
